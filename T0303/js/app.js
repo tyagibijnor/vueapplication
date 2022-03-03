97502aa39a8812111c32e72e28e5d138b8c0067c
tyagibijnor <tyagibijnor@gmail.com>
2022-03-03 21:30:11 +0530

    change js with vue syntax

--- T0303/js/app.js
+++ T0303/js/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        counter: 0
-      };
-    }    
-  });
-  app.mount("#events");
-  
+  data() {
+    return {
+      counter: 0,
+      inputValue:''
+    };
+  },
+  methods: {
+    addNum(num) {
+      this.counter = this.counter + num;
+    },
+    subNum(num) {
+      this.counter = this.counter - num;
+    },
+    inputVal(event){
+      this.inputValue = event.target.value;
+    }
+  }
+});
+app.mount("#events");
