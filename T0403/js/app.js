68ab5ca99c67725b7a2a24a565eee66a52fce461
tyagibijnor <tyagibijnor@gmail.com>
2022-03-04 11:48:02 +0530

    Added T0403

---
+++ T0403/js/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      inputValue:''
+    };
+  },
+  methods: {
+    addNum(num) {
+      this.counter = this.counter + num;
+    },
+    subNum(num) {
+      this.counter = this.counter - num;
+    },
+    inputVal(event){
+      this.inputValue = event.target.value;
+    },
+    resetValue(){
+      this.inputValue = '';
+    }
+  }
+});
+app.mount("#events");
+
