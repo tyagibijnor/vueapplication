60734501d35c1da0cb04663c43781fd8332e3c67
tyagibijnor <tyagibijnor@gmail.com>
2022-03-05 22:34:22 +0530

    css js copied

---
+++ T0603/js/app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        counter: 10,
+        inputVal: '',
+        outputVal: '',
+        fname: '',
+        lname: '',
+        fullName: ''
+      };
+    },
+    watch: {
+      // fname(value){        
+      //   // console.log(" Test ");
+      //   if ( value === '') {
+      //     this.fullName = '';
+      //   } else {
+      //     console.log("line18");
+      //     this.fullName = value + ' ' + this.lname;
+      //   } 
+      // },
+      // lname(value){
+      //   // console.log(" Test 2 ");
+      //   if ( value === '' ) {
+      //     this.fullName = '';
+      //   } else {
+      //     this.fullName = this.fname + ' ' + value;
+      //   }
+      // }
+    },
+    computed:{ 
+// showLog(){
+      //   console.log('Running conputed code');        
+      // },
+      // ShowOutPut(){
+      //   return this.outputVal;      
+      UserFullName(){        
+        console.log("In computed");
+       if ( this.fname === '' || this.lname === '') {
+         return '';
+       } 
+         return this.fname + ' ' + this.lname;
+       
+     }
+    },      
+    methods: {  
+      outputValue(event){
+        this.outputVal = event.target.value;
+        this.inputVal = '';
+
+      }
+    }
+  });
+  app.mount("#events");
+  
+  
