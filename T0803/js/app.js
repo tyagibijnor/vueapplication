02a012dc2f74118a22d0292816f2cbd8f523fa4b
tyagibijnor <tyagibijnor@gmail.com>
2022-03-08 22:34:01 +0530

    code for assignment

--- T0803/js/app.js
+++ T0803/js/app.js
@@ -1,10 +1,35 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      Result: 0,
+    };
+  },
+  computed: {
+    res() {
+      if (this.Result < 37) {
+        return "Not Yet";
+      } else if (this.Result === 37) {
+        return this.Result;
+      } else {
+        return "Too much";
+      }
+    },
+  },
+  watch: {
+    res() {
+      const that = this;
+      console.log(that.Result);
+      setTimeout(function () {
+        that.Result = 0;
+      }, 5000);
+    },
   },
   methods: {
     ShowHello() {
       alert("Hello Navneet");
+    },
+    addNum(num) {
+      this.Result = this.Result + num;
     },
   },
 });
