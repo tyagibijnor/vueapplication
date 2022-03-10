7cd28eff8ae80d81ba132fd3fafa4e5503950ed5
tyagibijnor <tyagibijnor@gmail.com>
2022-03-10 21:08:11 +0530

    data array for Goal

---
+++ addgoal/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+      return { 
+          enteredValue:'',
+          goals: [] 
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue)
+        }
+    },
+  });
+  
+  app.mount('#user-goals');
+  
